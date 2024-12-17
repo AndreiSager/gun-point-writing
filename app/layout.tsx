fa89ccfc7dbd0e35fe31401226cbd412c4d2a14d
AndreiSager <andreisager@proton.me>
2024-12-17 08:05:37 +0800

    style: center children

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col items-center">{children}</div>
       </body>
     </html>
   );
