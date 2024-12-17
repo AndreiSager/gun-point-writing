fe677d8cb3308d730f611dea5798149d3a686378
AndreiSager <andreisager@proton.me>
2024-12-17 08:06:11 +0800

    style: add title and textarea

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center border h-full">
-      <h1>Welcome to Gunpoint Writer!</h1>
+    <div className="flex-col flex justify-center items-center h-full max-w-[600px] w-full gap-20 py-16">
+      <h1 className="font-bold">Welcome to Gunpoint Writer!</h1>
+      <Textarea
+        placeholder="Type to start..."
+        className="resize-none min-h-[700px]"
+      />
     </div>
   );
 }
